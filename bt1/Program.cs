ab892f44b59b16377cc590666b8bf9e5053c1fa4
ledat9162 <ledat9162@gmail.com>
2024-01-22 09:49:05 +0700

    bo sung :xoa bat ki, max,tinh trung binh

--- bt1/Program.cs
+++ bt1/Program.cs
@@ -79,6 +79,65 @@ namespace bt1
                     }    
 
                 }
+                public void DeleteNote(int x)
+                {
+                    Node p = Head;
+                    Node q = null;
+                        while (p!=null && p.info !=x)
+                    {
+                        q = p;
+                        p = p.Next;
+                    }
+                        if(p !=null)
+                    {
+                        if (p == Head)
+                            DeleteHead();
+                        else
+                        {
+                            q.Next = p.Next;
+                            p.Next = null;
+                        }
+                    }
+                }
+                public Node FindMax()
+                {
+
+                    Node max = Head;
+                    Node p = Head.next;
+
+                    while (p != null)
+                    {
+                        if (p.info > max.info)
+                        {
+                            max = p;
+                        }
+
+                        p = p.next;
+                    }
+
+                    return max;
+                }
+
+                public float TinhTrungBinh()
+                {
+                    float sum = 0;
+                    float dem = 0;
+                    Node p = Head;
+                    while(p!=null)
+                    {
+                        sum +=p.info;
+                        dem++;
+                        p = p.next;
+
+                    }
+                    
+                        if (dem==0)
+                    {
+                        return 0;
+                    }
+                    return sum / dem;
+
+                }
                 public void  Xuat()
                 {
                     Node p = Head;
@@ -91,12 +150,7 @@ namespace bt1
                 static void Main(string[] args)
                 {
                     DSLienKetDon l = new DSLienKetDon();
-                    l.AddHead(9);
-                    l.AddHead(7);
-                    l.AddHead(8);
-                    l.Addlast(5);
-                    l.AddHead(10);
-                    l.Addlast(1);
+                    NhapDanhSach(l);
                     Console.WriteLine("Danh sach lien ket duoc tao:");
 
                     l.Xuat();
@@ -108,7 +162,35 @@ namespace bt1
                     l.Deletelast();
                     Console.WriteLine("Danh sach lien ket sau khi xoa nut cuoi:");
                     l.Xuat();
+
+                    
+                    Console.WriteLine("$Nhap gia tri x can xoa");
+                    int     x = int.Parse(Console.ReadLine());
+                    l.DeleteNote(x);
+                    Console.Write("\n Danh sach lien ket sau khi xoa nut co gia tri:");
+                    l.Xuat();
+
+                    Node max = l.FindMax();
+                    Console.WriteLine ($"Nut co gia tri lon nhat: { max.info}");
+
+                    float average = l.TinhTrungBinh();
+                    Console.WriteLine("Diem Trung Binh: " + average);
+                    l.Xuat();
+
                     Console.ReadLine();
+                }
+                static void NhapDanhSach(DSLienKetDon l)
+                {
+                    string chon = "y";
+                    int x;
+                    while (chon!="n")
+                    {
+                        Console.WriteLine("Nhap gia tri nut");
+                        x = int.Parse(Console.ReadLine());
+                        l.Addlast(x);
+                        Console.Write("Tiep tuc (y/n)?");
+                        chon = Console.ReadLine();
+                    }
                 }
             }
         }
